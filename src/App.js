d7ea66c2de508a8e55050beb3ac32c368afdeda4
Neutron02 <85206652+Neutron02@users.noreply.github.com>
2023-01-17 09:25:47 -0500

    Added pane and tile components, along with refactored selection page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import SelectionPage from './Components/SelectionPage'
 import TitlePage from './Components/TitlePage';
+import Tile from './Components/Tile';
+import Pane from './Components/Pane';
+
+const tileData = [
+  {title: 'Tile 1', popupText: 'Popup text for tile 1'},
+  {title: 'Tile 2', popupText: 'Popup text for tile 2'},
+  {title: 'Tile 3', popupText: 'Popup text for tile 3'},
+  {title: 'Tile 4', popupText: 'Popup text for tile 4'},
+  {title: 'Tile 5', popupText: 'Popup text for tile 5'},
+  
+];
+
 function App() {
-
-
   return (
     <div className="App">
       <TitlePage />
-      <SelectionPage />
+      <div id="selctions">
+        <Pane tilesData={tileData} />
+      </div>
     </div>
   );
 }
